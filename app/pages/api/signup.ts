e3efa70b9263fdda30428d9c167e9af48fabd0ba
1-EXON <exon.indiv@gmail.com>
2023-01-13 19:06:22 +0900

    Update: signup.ts

--- app/pages/api/signup.ts
+++ app/pages/api/signup.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         }
         const id = await db.get(`SELECT count(id) as count FROM user`)
         db.run(`INSERT INTO user VALUES (${id.count+1}, "${req.query.username}", "${req.query.password}")`)
-        res.status(200).json({ content: `${req.query.username}님 회원가입 되셨습니다.`, error: '' })
+        res.status(200).json({ content: `Hello ${req.query.username}. Welecome to my site`, error: '' })
     } else {
         res.status(404).json({ content: '', error: 'not exist url' })
     }
